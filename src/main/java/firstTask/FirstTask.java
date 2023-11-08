cc231ea3db74939e9930f0635a8346f5bb32a7f4
NikitaBay <n.bayushev2017@yandex.ru>
2023-11-09 01:39:54 +0400

    task 1 & 2

---
+++ src/main/java/firstTask/FirstTask.java
@@ -0,0 +1,28 @@
+package firstTask;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        int number = -12;
+        String result = number > 0 ? number % 7 < 4 ? "positive-m" : "positive-p" : number == 0 ? "zero" : number % 7 > -4 ? "negative-m" : "negative-p";
+        System.out.println(result);
+//        public static String convertNumber(int number) {
+//            String result;
+//            if (number > 0) {
+//                if (number % 7 < 4) {
+//                    result = "positive-m";
+//                } else {
+//                    result = "positive-p";
+//                }
+//            } else {
+//                if (number == 0) {
+//                    result = "zero";
+//                } else if (number % 7 > -4) {
+//                    result = "negative-m";
+//                } else {
+//                    result = "negative-p";
+//                }
+//            }
+//            return result;
+//        }
+    }
+}
---
+++ src/main/java/secondTask/SecondTask.java
@@ -0,0 +1,20 @@
+package secondTask;
+
+public class SecondTask {
+    private final String name = "Иван";
+    private String apples;
+
+    public void ivansApples(String name, int amount) {
+        if(amount % 10 == 1 && amount % 100 != 11) {
+            apples = "яблоко";
+        }
+        else if(amount % 10 >= 2 && amount % 10 <= 4 && (amount % 100 < 10 || amount % 100 >= 20)) {
+            apples = "яблока";
+        }
+        else {
+            apples = "яблок";
+        }
+        System.out.println(name + " хранит у себя " + amount + " " + apples);
+    }
+
+}
